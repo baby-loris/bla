c1ae61250dc2131517838d922a82f7d7969b7fcd
Filatov Dmitry <dfilatov@yandex-team.ru>
2019-12-19 09:56:56 +0300

    Simplify rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,60 +1,31 @@
 import typescript from 'rollup-plugin-typescript2';
 import * as pkg from './package.json';
 
-const rollupConfig = [
-    {
-        input: './src/server/index.ts',
-        output: [
-            {
-                name: pkg.name,
-                file: './server/index.js',
-                format: 'cjs'
+const rollupConfig = ['server', 'client'].map(env => ({
+    input: `./src/${env}/index.ts`,
+    output: [
+        {
+            name: pkg.name,
+            file: `./${env}/index.js`,
+            format: 'cjs'
+        }
+    ],
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+        typescript({
+            useTsconfigDeclarationDir: true,
+            tsconfigOverride: {
+                compilerOptions: {
+                    declaration: true,
+                    declarationDir: '.',
+                    module: 'esnext',
+                    target: env === 'client' ? 'es5' : 'es2017'
+                },
+                include: [`src/${env}/**/*`],
+                exclude: [`src/${env}/__tests__`]
             }
-        ],
-        external: Object.keys(pkg.dependencies),
-        plugins: [
-            typescript({
-                useTsconfigDeclarationDir: true,
-                tsconfigOverride: {
-                    compilerOptions: {
-                        allowJs: false,
-                        declaration: true,
-                        declarationDir: '.',
-                        module: 'esnext'
-                    },
-                    include: ['src/server/**/*'],
-                    exclude: ['src/server/__tests__']
-                }
-            })
-        ]
-    },
-    {
-        input: './src/client/index.ts',
-        output: [
-            {
-                name: pkg.name,
-                file: './client/index.js',
-                format: 'cjs'
-            }
-        ],
-        external: Object.keys(pkg.dependencies),
-        plugins: [
-            typescript({
-                useTsconfigDeclarationDir: true,
-                tsconfigOverride: {
-                    compilerOptions: {
-                        allowJs: false,
-                        declaration: true,
-                        declarationDir: '.',
-                        module: 'esnext',
-                        target: 'es5'
-                    },
-                    include: ['src/client/**/*'],
-                    exclude: ['src/client/__tests__']
-                }
-            })
-        ]
-    }
-];
+        })
+    ]
+}));
 
 export default rollupConfig;
