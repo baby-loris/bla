be2130bc41a81040ebfd9d3ffaee347d055389aa
Filatov Dmitry <dfilatov@yandex-team.ru>
2019-12-23 16:05:09 +0300

    Fix ApiError inheritance

--- src/shared/ApiError.ts
+++ src/shared/ApiError.ts
@@ -1,13 +1,38 @@
-class ApiError<TSource = unknown> extends Error {
-    name: 'ApiError';
-
-    constructor(
-        message: string,
-        public readonly source?: TSource
-    ) {
-        super(message);
-        this.name = 'ApiError';
-    }
+interface ApiError<TSource = unknown> extends Error {
+    source?: TSource;
 }
+
+declare class ApiError<TSource extends unknown> {
+    constructor(message: string, source?: TSource);
+}
+
+function ApiError<TSource extends unknown>(this: unknown, message: string, source?: TSource): ApiError<TSource> {
+    const err = new Error(message) as ApiError<TSource>;
+
+    err.name = 'ApiError';
+    err.source = source;
+
+    Object.setPrototypeOf(err, Object.getPrototypeOf(this));
+
+    if(Error.captureStackTrace) {
+        Error.captureStackTrace(err, ApiError);
+    }
+
+    return err;
+};
+
+ApiError.prototype = Object.create(
+    Error.prototype,
+    {
+        constructor: {
+            value: Error,
+            enumerable: false,
+            writable: true,
+            configurable: true
+        }
+    }
+);
+
+Object.setPrototypeOf(ApiError, Error);
 
 export default ApiError;
