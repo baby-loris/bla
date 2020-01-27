6cbe6e4432f1d169d7d55a8d107a5b343bc4a08c
Filatov Dmitry <dfilatov@yandex-team.ru>
2020-01-27 10:45:38 +0300

    Expose some types  Please enter the commit message for your changes. Lines starting

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,18 @@
 import Api, { ExtractApiContract } from './Api';
-import ApiMethod from './ApiMethod';
+import ApiMethod, { ApiMethodParams, ExtractApiMethodParams, ExtractApiMethodResult } from './ApiMethod';
 import apiMiddleware from './apiMiddleware';
 import ApiError from '../shared/ApiError';
 import { ApiContract } from '../shared/types';
 
-export { Api, ApiMethod, ApiError, apiMiddleware, ApiContract, ExtractApiContract };
+export {
+    Api,
+    ApiContract,
+    ApiMethod,
+    ApiMethodParams,
+    ApiError,
+    apiMiddleware,
+
+    ExtractApiContract,
+    ExtractApiMethodParams,
+    ExtractApiMethodResult
+};
